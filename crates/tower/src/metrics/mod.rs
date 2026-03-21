8e254fa04adf29cdf17670664a07df5f5216a0b2
Chrislearn Young <chris@acroidea.com>
2026-03-21 12:34:22 +0800

    Remove pasion-tower crate and its dependencies
    - Deleted the `pasion-tower` crate from the workspace and removed its dependencies from `Cargo.toml` files.
    - Removed all related source files and modules associated with `pasion-tower`, including metrics, tracing, and utility functions.
    - Updated the `Cargo.toml` files in the main project and the `cli` crate to reflect the removal of `pasion-tower`.
    
    Signed-off-by: Chrislearn Young <chris@acroidea.com>

--- crates/tower/src/metrics/mod.rs
+++
@@ -1,9 +0,0 @@
-mod duration;
-mod in_flight;
-mod make_attributes;
-
-pub use self::{
-    duration::{DurationRecorderFuture, DurationRecorderLayer, DurationRecorderService},
-    in_flight::{InFlightCounterLayer, InFlightCounterService, InFlightFuture},
-    make_attributes::{MetricsAttributes, metrics_attributes_fn},
-};
--- misc/update.sh
+++ misc/update.sh
@@ -5,16 +5,10 @@ export SQLX_OFFLINE=1
 BASE_DIR="$(dirname "$0")/.."
 CONFIG_SCHEMA="${BASE_DIR}/docs/config.schema.json"
 API_SCHEMA="${BASE_DIR}/docs/api/spec.json"
-GRAPHQL_SCHEMA="${BASE_DIR}/frontend/schema.graphql"
 POLICIES_SCHEMA="${BASE_DIR}/policies/schema/"
 
 set -x
 cargo run -p pasion-config > "${CONFIG_SCHEMA}"
-cargo run -p pasion-handlers --bin graphql-schema > "${GRAPHQL_SCHEMA}"
 cargo run -p pasion-handlers --bin api-schema > "${API_SCHEMA}"
 cargo run -p pasion-i18n-scan -- --update "${BASE_DIR}/templates/" "${BASE_DIR}/translations/en.json"
 OUT_DIR="${POLICIES_SCHEMA}" cargo run -p pasion-policy
-
-cd "${BASE_DIR}/frontend"
-npm run format
-npm run generate
